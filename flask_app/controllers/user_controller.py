9f1f45dd4623a4e436d85ffa73f7004859f9b9ad
John Tran <namesjohntran@gmail.com>
2023-09-06 17:25:21 -0700

    commit

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -2,8 +2,13 @@ from flask_app import app
 from flask import render_template, redirect, request, session, flash
 
 @app.route('/')
-def home():
+def login():
     if 'userid' in session:
         return redirect('/dashboard')    # if user logged in send to dashboard
 
     return render_template('login_page.html')
+
+
+@app.route('/dashboard')
+def homepage():
+    return render_template('dashboard.html')
