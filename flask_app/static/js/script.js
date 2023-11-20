1c246e700508076d5b707d947492fcee62c0075a
John Tran <namesjohntran@gmail.com>
2023-11-19 22:15:00 -0800

    commit

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -26,56 +26,86 @@ const swiper = new Swiper(".swiper", {
 });
 
 function scrollToArticles() {
-    var articleContainer = document.getElementById('articles-container');
-    articleContainer.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    var articleContainer = document.getElementById("articles-container");
+    articleContainer.scrollIntoView({ behavior: "smooth", block: "start" });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const replyButtons = document.querySelectorAll('.reply-button');
-    const replyForms = document.querySelectorAll('.reply-form');
+document.addEventListener("DOMContentLoaded", function () {
+    const replyButtons = document.querySelectorAll(".reply-button");
+    const replyForms = document.querySelectorAll(".reply-form");
 
     replyButtons.forEach((button, index) => {
-        button.addEventListener('click', function () {
+        button.addEventListener("click", function () {
             const replyForm = replyForms[index];
-            replyForm.style.display = replyForm.style.display === 'none' ? 'block' : 'none';
-        })
-    })
-
-
-
+            replyForm.style.display =
+                replyForm.style.display === "none" ? "block" : "none";
+        });
+    });
 });
 
-var articleLikedCount = document.getElementById('article-count').textContent;
+var articleLikedCount = document.getElementById("article-count").textContent;
 console.log(articleLikedCount);
-var likeForms = document.querySelectorAll('.likeForm');
+var likeForms = document.querySelectorAll(".likeForm");
 
-likeForms.forEach(function(likeForm){
+likeForms.forEach(function (likeForm) {
     likeForm.onsubmit = function (e) {
         e.preventDefault();
         var likedInput = likeForm.querySelector('input[name="liked"]');
         var formData = new FormData(likeForm);
         console.log(likedInput);
-        fetch("http://localhost:5000/articles/dashboard/like", { method: 'POST', body: formData })
-            .then(response => response.json())
-            .then(data => {
+        fetch("http://localhost:5000/articles/dashboard/like", {
+            method: "POST",
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((data) => {
                 console.log(articleLikedCount);
-                var likesCount = likeForm.querySelector('.likes-count');
-                var thumbsUp = likeForm.querySelector('.thumb');
+                var likesCount = likeForm.querySelector(".likes-count");
+                var thumbsUp = likeForm.querySelector(".thumb");
                 likesCount.textContent = data.likes_count;
-                thumbsUp.className = thumbsUp.className === 'fa-solid fa-thumbs-up thumb' ? 'fa-regular fa-thumbs-up thumb': 'fa-solid fa-thumbs-up thumb';
-                if(likedInput.value === 'FALSE'){
+                thumbsUp.className =
+                    thumbsUp.className === "fa-solid fa-thumbs-up thumb"
+                        ? "fa-regular fa-thumbs-up thumb"
+                        : "fa-solid fa-thumbs-up thumb";
+                if (likedInput.value === "FALSE") {
                     articleLikedCount++;
-                }else{
+                } else {
                     articleLikedCount -= 1;
                 }
-                document.getElementById('article-count').textContent = articleLikedCount;
-                console.log('value ', likedInput.value)
-                likedInput.value = likedInput.value === 'TRUE' ? 'FALSE' : 'TRUE';
+                document.getElementById("article-count").textContent =
+                    articleLikedCount;
+                console.log("value ", likedInput.value);
+                likedInput.value =
+                    likedInput.value === "TRUE" ? "FALSE" : "TRUE";
             })
-            .catch(err => {console.log(err)})
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+});
+
+// Get the modal
+var modal = document.getElementById("modal-links");
+
+// Get the button that opens the modal
+var btn = document.getElementById("menu-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-
-})
-
-
-
+};
